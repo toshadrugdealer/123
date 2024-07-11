6822a8a7384f2f46a4347634a18b690453aa8948
toshadrugdealer <t.reikh@yandex.ru>
2024-07-11 15:53:42 +0300

    add skeleton news/banner

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,28 +1,41 @@
-import styles from "./styles.module.css";
-import { NewsBanner } from "../../components/NewsBanner/NewsBanner";
 import { useEffect, useState } from "react";
 import { getNews } from "../../api/apiNews";
+import styles from "./styles.module.css";
+import { NewsBanner } from "../../components/NewsBanner/NewsBanner";
 import { NewsList } from "../../components/NewsList/NewsList";
+import { Skeleton } from "../../components/Sleleton/Skeleton";
 export function Main() {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true);
         const response = await getNews();
         setNews(response.news);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
+
     fetchNews();
   }, []);
 
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[1]} /> : null}
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[0]} />
+      ) : (
+        <Skeleton type={"banner"} count={1} />
+      )}
 
-      <NewsList news={news} />
+      {!isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Skeleton type={"item"} count={10} />
+      )}
     </main>
   );
 }
