bc38c268c35ad6becacb5abae148257f2e6ff145
toshadrugdealer <t.reikh@yandex.ru>
2024-07-11 17:48:42 +0300

    fixed date in safari

--- src/helpers/formatTimeAgo.js
+++ src/helpers/formatTimeAgo.js
@@ -1,6 +1,13 @@
 export const formatDateAgo = (dateString) => {
+  const values = dateString.split(/[^0-9]/);
+  const year = parseInt(values[0], 10);
+  const month = parseInt(values[1], 10) - 1;
+  const day = parseInt(values[2], 10);
+  const hours = parseInt(values[3], 10);
+  const minutes = parseInt(values[4], 10);
+  const seconds = parseInt(values[5], 10);
   const now = new Date();
-  const date = new Date(dateString);
+  const date = new Date(year, month, day, hours, minutes, seconds);
   const secondPast = (now.getTime() - date.getTime()) / 1000;
   if (secondPast < 60) {
     return `${Math.floor(secondPast)}s ago`;
