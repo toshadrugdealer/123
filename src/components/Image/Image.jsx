da0e7d90a984997695c5704a6496eae67a78b696
toshadrugdealer <t.reikh@yandex.ru>
2024-07-11 16:19:24 +0300

    skeleton fix

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -2,7 +2,13 @@ import styles from "./styles.module.css";
 export function Image({ image }) {
   return (
     <div className={styles.wrapper}>
-      {image ? <img src={image} alt="news" className={styles.image} /> : null}
+      {image ? (
+        <img
+          src={image}
+          alt="изображение отсутствует"
+          className={styles.image}
+        />
+      ) : null}
     </div>
   );
 }
