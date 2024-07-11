4458805c557e29cbf52d11ba6331ad90bd180245
toshadrugdealer <t.reikh@yandex.ru>
2024-07-11 18:16:46 +0300

    adding placeholders to empty spaces for images

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,11 +1,12 @@
 import { formatDateAgo } from "../../helpers/formatTimeAgo";
 import { Image } from "./../Image/Image";
 import styles from "./styles.module.css";
+import noLogo from "./../../assets/noLogo.png";
 
 export function NewsBanner({ item }) {
   return (
     <div className={styles.banner}>
-      <Image image={item?.image} />
+      <Image image={item.image === "None" ? noLogo : item.image} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
         {formatDateAgo(item.published)} by {item.author}
--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,11 +1,16 @@
 import { formatDateAgo } from "../../helpers/formatTimeAgo";
 import styles from "./styles.module.css";
+import noLogo from "./../../assets/noLogo.png";
 export function NewsItem({ item }) {
   return (
     <li className={styles.item}>
       <div
         className={styles.wrapper}
-        style={{ backgroundImage: `url(${item.image})` }}
+        style={{
+          backgroundImage: `url(${
+            item.image === "None" ? noLogo : item.image
+          })`,
+        }}
       ></div>
       <div className={styles.info}>
         <h3 className={styles.title}>{item.title}</h3>
